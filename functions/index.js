51eaccb4573a32a819f838320218e326b07e107c
jrwishart93 <jrwishart@hotmail.co.uk>
2025-06-19 00:20:29 +0100

    feat: add scheduled cleanup for expired reports

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,5 @@
 const sendEmail = require('../firebase/functions/sendEmail');
+const cleanupExpiredReports = require('../firebase/functions/cleanupExpiredReports');
+
 exports.sendEmail = sendEmail.sendEmail;
+exports.cleanupExpiredReports = cleanupExpiredReports.cleanupExpiredReports;
