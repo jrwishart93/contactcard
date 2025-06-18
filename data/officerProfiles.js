c0edc9ad5b70a0a518febbf84845bff0df3d58b8
jrwishart93 <jrwishart@hotmail.co.uk>
2025-06-19 00:41:22 +0100

    Convert new pages to JS

---
+++ data/officerProfiles.js
@@ -0,0 +1,20 @@
+export const officerProfiles = [
+  {
+    id: 'john-smith',
+    name: 'PC John Smith',
+    badgeNumber: '1232342',
+    collarNumber: 'T123',
+    unit: 'Road Policing Unit',
+    email: 'folk_dragnet.2a@icloud.com',
+    profileImage: '/images/trafficofficercontact.png',
+  },
+  {
+    id: 'anna-smith',
+    name: 'PC Anna Smith',
+    badgeNumber: '654321',
+    collarNumber: 'R456',
+    unit: 'Community Policing',
+    email: 'anna.smith@scotland.police.uk',
+    profileImage: '/images/trafficofficercontact.png',
+  },
+];
