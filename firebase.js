c95dc62b2d83a1292767747896eba70b7f467cc7
jrwishart93 <jrwishart@hotmail.co.uk>
2025-06-19 21:01:17 +0100

    use env vars for firebase config

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from 'firebase/analytics';
 import { getFirestore, setLogLevel } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNBCnQYJAykquj_bVcXOdKfeU0So3Depc",
-  authDomain: "contact-cards-b9175.firebaseapp.com",
-  projectId: "contact-cards-b9175",
-  storageBucket: "contact-cards-b9175.appspot.com",
-  messagingSenderId: "1083444636856",
-  appId: "1:1083444636856:web:82a43f44cf921dc9eb184d",
-  measurementId: "G-EMC2XRVL0C"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
